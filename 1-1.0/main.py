826805cd5fe2664604746a13ef60d096cabd2c0e
dodam2015 <145336038+dodam2015@users.noreply.github.com>
2025-05-11 10:20:52 +0900

    1-1.0

---
+++ 1-1.0/main.py
@@ -0,0 +1,22 @@
+print('made by L.Doyun')
+import time
+print('loaded')
+
+time.sleep(2)
+
+level=0
+money=0
+print('status reseted!')
+
+cmd=''
+while True:
+    print('1.see your status.')
+    print('2.go to the store.')
+    print('3.save and exit.')
+    cmd=input(':')
+    if cmd==1:
+        print('1')
+    elif cmd==2:
+        print('2')
+    elif cmd==3:
+        print('3')
